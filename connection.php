fcd6ff4cc85fbd038795b489cd9b884c2df86153
Saiful Islam <codersaiful@gmail.com>
2016-11-10 17:18:52 +0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,4 +6,10 @@ $db_name = 'chat';
 
 
 $conn = new mysqli($host,$user,$password,$db_name);
+/*
+Here use your own Database information.
+I have just made at localhost
+database name was: chat
+you can also change this name
+*/
 ?>
